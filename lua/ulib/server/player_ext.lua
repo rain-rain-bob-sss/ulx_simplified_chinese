a6a863bfb9d343c0d2912aeb0e7205060c13650e
波と一緒に漂う <2247564152@qq.com>
2024-08-23 11:06:15 +0800

    update

--- lua/ulib/server/player_ext.lua
+++ lua/ulib/server/player_ext.lua
@@ -1,76 +1,72 @@
-local meta = FindMetaTable("Player")
+local meta = FindMetaTable( "Player" )
 
 ULib.spawnWhitelist = -- Tool white list for tools that don't spawn things
 {
-    "colour",
-    "material",
-    "paint",
-    "ballsocket",
-    "ballsocket_adv",
-    "weld",
-    "keepupright",
-    "nocollide",
-    "eyeposer",
-    "faceposer",
-    "statue",
-    "weld_ez",
-    "axis",
+	"colour",
+	"material",
+	"paint",
+	"ballsocket",
+	"ballsocket_adv",
+	"weld",
+	"keepupright",
+	"nocollide",
+	"eyeposer",
+	"faceposer",
+	"statue",
+	"weld_ez",
+	"axis",
 }
 
 -- Return if there's nothing to add on to
 if not meta then return end
 
-function meta:DisallowNoclip(bool)
-    self.NoNoclip = bool
+function meta:DisallowNoclip( bool )
+	self.NoNoclip = bool
 end
 
-function meta:DisallowSpawning(bool)
-    self.NoSpawning = bool
+function meta:DisallowSpawning( bool )
+	self.NoSpawning = bool
 end
 
-function meta:DisallowVehicles(bool)
-    self.NoVehicles = bool
+function meta:DisallowVehicles( bool )
+	self.NoVehicles = bool
 end
 
-local function tool(ply, tr, toolmode)
-    if not ply or not ply:IsValid() then return end
+local function tool( ply, tr, toolmode )
+	if not ply or not ply:IsValid() then return end
 
-    if ply.NoSpawning then
-        if not table.HasValue(ULib.spawnWhitelist, toolmode) then
-            return false
-        end
-    end
+	if ply.NoSpawning then
+		if not table.HasValue( ULib.spawnWhitelist, toolmode ) then
+			return false
+		end
+	end
 end
+hook.Add( "CanTool", "ULibPlayerToolCheck", tool, HOOK_HIGH )
 
-hook.Add("CanTool", "ULibPlayerToolCheck", tool, HOOK_HIGH)
-
-local function noclip(ply)
-    if not ply or not ply:IsValid() then return end
-    if ply.NoNoclip then return false end
+local function noclip( ply )
+	if not ply or not ply:IsValid() then return end
+	if ply.NoNoclip then return false end
 end
+hook.Add( "PlayerNoClip", "ULibNoclipCheck", noclip, HOOK_HIGH )
 
-hook.Add("PlayerNoClip", "ULibNoclipCheck", noclip, HOOK_HIGH)
-
-local function spawnblock(ply)
-    if not ply or not ply:IsValid() then return end
-    if ply.NoSpawning then return false end
+local function spawnblock( ply )
+	if not ply or not ply:IsValid() then return end
+	if ply.NoSpawning then return false end
 end
+hook.Add( "PlayerSpawnObject", "ULibSpawnBlock", spawnblock )
+hook.Add( "PlayerSpawnEffect", "ULibSpawnBlock", spawnblock )
+hook.Add( "PlayerSpawnProp", "ULibSpawnBlock", spawnblock )
+hook.Add( "PlayerSpawnNPC", "ULibSpawnBlock", spawnblock )
+hook.Add( "PlayerSpawnVehicle", "ULibSpawnBlock", spawnblock )
+hook.Add( "PlayerSpawnRagdoll", "ULibSpawnBlock", spawnblock )
+hook.Add( "PlayerSpawnSENT", "ULibSpawnBlock", spawnblock )
+hook.Add( "PlayerGiveSWEP", "ULibSpawnBlock", spawnblock )
 
-hook.Add("PlayerSpawnObject", "ULibSpawnBlock", spawnblock)
-hook.Add("PlayerSpawnEffect", "ULibSpawnBlock", spawnblock)
-hook.Add("PlayerSpawnProp", "ULibSpawnBlock", spawnblock)
-hook.Add("PlayerSpawnNPC", "ULibSpawnBlock", spawnblock)
-hook.Add("PlayerSpawnVehicle", "ULibSpawnBlock", spawnblock)
-hook.Add("PlayerSpawnRagdoll", "ULibSpawnBlock", spawnblock)
-hook.Add("PlayerSpawnSENT", "ULibSpawnBlock", spawnblock)
-hook.Add("PlayerGiveSWEP", "ULibSpawnBlock", spawnblock)
-
-local function vehicleblock(ply, ent)
-    if not ply or not ply:IsValid() then return end
-    if ply.NoVehicles then
-        return false
-    end
+local function vehicleblock( ply, ent )
+	if not ply or not ply:IsValid() then return end
+	if ply.NoVehicles then
+		return false
+	end
 end
-
-hook.Add("CanPlayerEnterVehicle", "ULibVehicleBlock", vehicleblock, HOOK_HIGH)
-hook.Add("CanDrive", "ULibVehicleDriveBlock", vehicleblock, HOOK_HIGH)
+hook.Add( "CanPlayerEnterVehicle", "ULibVehicleBlock", vehicleblock, HOOK_HIGH )
+hook.Add( "CanDrive", "ULibVehicleDriveBlock", vehicleblock, HOOK_HIGH )
